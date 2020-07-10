7c4885b976d0247f46c2b83991d79a0f61661cf9
Narges <narges.ghaedibardeh@Narges.GhaediBardeh@student.hpi.uni-potsdam.de>
2020-07-10 19:18:10 +0430

    stat

---
+++ src/models/statModels.py
@@ -0,0 +1,168 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime as dt
+import plotly.offline as py
+import plotly.graph_objs as go
+from plotly.offline import init_notebook_mode
+
+from plotly import tools
+
+
+dataset= pd.read_csv('e:/avocados/dl_q2_avocadopriceprediction/dataset/avocado.csv')#, parse_dates=["Date"])
+
+
+dataset['Date']=pd.to_datetime(dataset['Date'])
+dataset['Month']=dataset['Date'].apply(lambda x:x.month)
+dataset['Day']=dataset['Date'].apply(lambda x:x.day)
+
+#print(dataset.head())
+
+#Average Price
+byDate=dataset.groupby('Date').mean()
+plt.figure(figsize=(17,8),dpi=100)
+byDate['AveragePrice'].plot()
+plt.title('Average Price')
+
+#Average Price Per Month
+byMonth = dataset.groupby("Month").mean()
+#plt.figure(figsize=(17,8),dpi=100)
+#plt.plot(["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sept","Oct","Nov","Dec"],byMonth['AveragePrice'])
+#plt.title('Average Price Per Month')
+
+#Average Price Per Day
+byDay = dataset.groupby("Day").mean()
+#plt.figure(figsize=(17,8),dpi=250)
+#byDay['AveragePrice'].plot()
+#plt.title('Average Price Per Day')
+
+
+#Average Price According to Region
+byRegion=dataset.groupby('region').mean()
+byRegion.sort_values(by=['AveragePrice'], ascending=False, inplace=True)
+#plt.figure(figsize=(17,8),dpi=250)
+#sns.barplot(x = byRegion.index,y=byRegion["AveragePrice"],data = byRegion,palette='rocket')
+#plt.xticks(rotation=90)
+#plt.xlabel('Region')
+#plt.ylabel('Average Price')
+#plt.title('Average Price According to Region')
+
+
+#correlation
+corr_df = dataset.corr(method='pearson')
+#plt.figure(figsize=(12,6),dpi=100)
+#sns.heatmap(corr_df,cmap='coolwarm',annot=True)
+
+#outliers
+plt.figure(figsize=(15,7),dpi=100)
+sns.boxplot(data = dataset[[
+ 'AveragePrice',
+ 'Total Volume',
+ '4046',
+ '4225',
+ '4770',
+ 'Total Bags',
+ 'Small Bags',
+ 'Large Bags',
+ 'XLarge Bags']])
+
+
+dataset.drop(columns=["Date"],inplace=True)
+dataset.info()
+
+
+#remove outliers
+from numpy import percentile
+
+columns = dataset.columns
+for j in columns:
+    if isinstance(dataset[j][0], str) :
+        continue
+    else:
+        for i in range(len(dataset)):
+            #defining quartiles
+            quartiles = percentile(dataset[j], [25,75])
+            # calculate lower/upper whisker
+            lower_fence = quartiles[0] - (1.5*(quartiles[1]-quartiles[0]))
+            upper_fence = quartiles[1] + (1.5*(quartiles[1]-quartiles[0]))
+            if dataset[j][i] > upper_fence:
+                dataset[j][i] = upper_fence
+            elif dataset[j][i] < lower_fence:
+                dataset[j][i] = lower_fence
+
+dataset.head()
+
+dataset['region'] = pd.Categorical(dataset['region'])
+dfDummies_region = pd.get_dummies(dataset['region'], prefix = 'region')
+
+dataset = pd.concat([dataset, dfDummies_region], axis=1)
+dataset.drop(columns="region",inplace=True)
+
+dataset['Month'] = pd.Categorical(dataset['Month'])
+dfDummies_month = pd.get_dummies(dataset['Month'], prefix = 'month')
+
+dataset = pd.concat([dataset, dfDummies_month], axis=1)
+dataset.drop(columns="Month",inplace=True)
+
+from sklearn import preprocessing 
+ 
+label_encoder = preprocessing.LabelEncoder() 
+dataset['type']= label_encoder.fit_transform(dataset['type']) 
+dataset
+
+
+X=dataset.iloc[:,1:78]
+y=dataset['AveragePrice']
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=50)
+y_test = np.array(y_test,dtype = float)
+
+from sklearn.preprocessing import StandardScaler
+sc=StandardScaler()
+X_train=sc.fit_transform(X_train)
+X_test=sc.transform(X_test)
+
+
+import sklearn.metrics as metrics
+
+def regression_results(y_true, y_pred):
+    explained_variance=metrics.explained_variance_score(y_true, y_pred)
+    mean_absolute_error=metrics.mean_absolute_error(y_true, y_pred) 
+    mse=metrics.mean_squared_error(y_true, y_pred) 
+    r2=metrics.r2_score(y_true, y_pred)
+    adjusted_r2 = 1 - (1-r2)*(len(y_true)-1)/(len(y_true)-X_test.shape[1]-1)
+
+    print('Explained_variance: ', round(explained_variance,4))    
+    print('R2: ', round(r2,4))
+    print('Adjusted_r2: ', round(adjusted_r2,4))
+    print('MAE: ', round(mean_absolute_error,4))
+    print('MSE: ', round(mse,4))
+    print('RMSE: ', round(np.sqrt(mse),4))
+
+from sklearn.model_selection import cross_val_score
+def model_accuracy(model,X_train=X_train,y_train=y_train):
+    accuracies = cross_val_score(estimator = model, X = X_train, y = y_train, cv = 10)
+    print("Accuracy: {:.2f} %".format(accuracies.mean()*100))
+    print("Standard Deviation: {:.2f} %".format(accuracies.std()*100))
+
+from sklearn.linear_model import LinearRegression
+import statsmodels.api as sm
+
+regressor=LinearRegression()
+regressor.fit(X_train,y_train)
+y_pred = regressor.predict(X_test)
+regression_results(y_test,y_pred)
+model_accuracy(regressor)
+
+plt.figure(figsize=(16, 12),dpi=50)
+red = plt.scatter(range(len(X_test)),y_pred,c='r',s = 10)
+blue = plt.scatter(range(len(X_test)),y_test,c='b', s = 10)
+plt.title("Scatter Plot of y_pred and y_test for Regression",fontsize=15)
+plt.legend((red,blue),("y_pred","y_test"),scatterpoints=1, loc='upper right',fontsize=12)
+
+
+
+
+
+plt.show()
