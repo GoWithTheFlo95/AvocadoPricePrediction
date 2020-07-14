356e055b1b3c6d80f467dad7a47730c812762731
Florian Hermes <florian.hermes@student.hpi.uni-potsdam.de>
2020-07-14 21:07:51 +0200

    Add first Base, RNN and LSTM model

--- src/models/model_dl.py
+++ src/models/model_dl.py
@@ -16,3 +16,80 @@ import pandas as pd
 home = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, home+'/../../dataset')
 sys.path.insert(0, home+'/../')
+
+import torch
+import torch.nn as nn
+import torch.autograd as autograd
+from torch.autograd import Variable
+
+
+
+## BaseModel
+class BaseModel(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, sequence_len, n_layer, cell, cuda=False):
+
+        super(BaseModel, self).__init__()
+        
+        self.hidden_size = hidden_size          # number of neurons that store information of previous inputs
+        self.input_size = input_size            # size of a input element of a sequence
+        self.output_size = output_size          # size of a output element
+        self.sequence_len = sequence_len        # length of a sequence
+        self.n_layer = n_layer                  # num of RNNs stacked on top of each other
+
+        if cell == "RNN":
+            self.model = nn.RNN(input_size = self.input_size, hidden_size = self.hidden_size, num_layers = self.n_layer, batch_first = True)
+        
+        if cell == "LSTM":
+            self.model = nn.LSTM(input_size = self.input_size, hidden_size = self.hidden_size, num_layers = self.n_layer, batch_first = True)
+        
+        print(self.model)
+        self.linear = nn.Linear(self.hidden_size, self.output_size)
+
+    def init_hidden(self, batch_size):
+        hidden = Variable(torch.zeros(self.n_layer * 1, batch_size, self.hidden_size))
+        return hidden
+
+# RNNModel
+class RNNModel(BaseModel):
+    def __init__(self, input_size, hidden_size, output_size, sequence_len, n_layer, cell, cuda):
+
+        super(RNNModel, self).__init__(input_size, hidden_size, output_size, sequence_len, n_layer, cell, cuda)
+
+    def forward(self, input):
+        batch_size = input.size(0)
+        
+        h0 = self.init_hidden(batch_size)
+
+        if self.cuda:
+            h0 = h0.cuda()
+        
+        rnnOut, hn = self.model(input, h0)
+        hn = hn.view(batch_size, self.hidden_size)
+
+        linearOut = self.linear(hn)
+
+        return linearOut
+
+# LSTMModel
+class LSTMModel(BaseModel):
+    def __init__(self, input_size, hidden_size, output_size, sequence_len, n_layer, cell, cuda):
+
+        super(LSTMModel, self).__init__(input_size, hidden_size, output_size, sequence_len, n_layer, cell, cuda)
+    
+    def forward(self, input):
+        batch_size = input.size(0)
+
+        h0 = self.init_hidden(batch_size)
+        c0 = self.init_hidden(batch_size)
+
+        if self.cuda:
+            h0 = h0.cuda()
+            c0 = c0.cuda()
+        
+        lstmOut, hn = self.model(input, (h0, c0))
+        hn = hn[0].view(batch_size, self.hidden_size)
+
+        linearOut = self.linear(hn)
+
+        return linearOut
+
