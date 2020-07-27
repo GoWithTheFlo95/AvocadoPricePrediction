28151dcd8e096e8d704d099a5d8153ea94a08a79
Florian Hermes <florian.hermes@student.hpi.uni-potsdam.de>
2020-07-27 14:18:29 +0200

    Adjust preparation of train dataset

--- src/train.py
+++ src/train.py
@@ -27,14 +27,9 @@ def train(dataset, n_epochs, optimizer, model, loss_function):
         for features, labels in dataset:
             optimizer.zero_grad()
 
-            inputs = features.values.reshape(1, -1, 1)
-            inputs = torch.Tensor(inputs)
+            inputs = features.reshape(1, -1, 1)
 
-            print(inputs.ndim)
-            print(inputs.size)
-            print(inputs.shape[0])
-
-            _, hidden = model(inputs)
+            hidden = model(inputs)
 
             loss = loss_function(hidden, labels)
             loss.backward()
@@ -59,55 +54,47 @@ import math
 
 data = getData()
 
-data = data.drop(['Date', 'SmallHass', 'LargeHass', 'XLargeHass', 'TotalBags', 'SmallBags', 'LargeBags', 'XLargeBags', 'type', 'year'], 1)
-data = data.loc[:, 'AveragePrice']
-print(data)
-
-#batch_size = data.shape[0] / data['region'].nunique()
-#print(batch_size)
-#print(18250/54)
-
+data = data.drop(['Date', 'TotalVol', 'SmallHass', 'LargeHass', 'XLargeHass', 'TotalBags', 'SmallBags', 'LargeBags', 'XLargeBags', 'type', 'year'], 1)
 data = data.drop( data[data.region == 'WestTexNewMexico'].index )
+print(data.info())
 
-for i in range(data['region'].nunique()):
-    count = data.loc[data['region'] == data['region'].unique()[i], 'region'].count()
-    if count != 338:
-        print(data['region'].unique()[i])
-        print(count)
 
-# 338 timestemps for 53 regions with batch_size 1 and 1 feature
-train_seq = []
-test_seq = []
-#data_seq = []
-
-count = 338 #data.loc[data['region'] == data['region'].unique()[i], 'region'].count()
-count_train = math.floor(count * 0.8)
-slide_win = 52
-#label_index = slide_win+1
-
-for i in range(data['region'].nunique()):
-    features = data.loc[data['region'] == data['region'].unique()[i], 'AveragePrice']
-    label = data.loc[data['region'] == data['region'].unique()[i], 'AveragePrice']
-    train_seq.append((features[:count_train], label[:count_train]))
-    test_seq.append((features[count_train:], label[count_train:]))
-    #data_seq.append((features, label))
-
-# 52 week for timetemps
-
-print(features)
-
-#print(type(features))
-
-# initialize
-print('INITIALIZATION')
+## Initialization
+# hyperparameters 
 lr = 0.01
 n_epochs = 100
+hidden_size = 50
+slide_win = 52
 
-lstm_model = LSTMModel(input_size=1, hidden_size=50, output_size=1, n_layer=1, sequence_len=1, cell = "LSTM")
+lstm_model = LSTMModel(input_size=1, hidden_size=hidden_size, output_size=1, n_layer=1, sequence_len=1, cell = "LSTM")
 loss_function = nn.MSELoss() # or L1 loss           #CrossEntropy for Categories
 optimizer = torch.optim.Adam(lstm_model.parameters(), lr=lr)
 
-train(train_seq, n_epochs, optimizer, lstm_model, loss_function)
+
+# Dataset preparation
+# 338 timestemps for 53 regions with batch_size 1 and 1 feature
+train_seq = []
+test_seq = []
+
+count = data.loc[data['region'] == data['region'].unique()[0], 'region'].count() #338
+count_train = math.floor(count * 0.8) # train, test index
+
+i=0
+
+for i in range(data['region'].nunique()):
+    avgPofReg = data.loc[data['region'] == data['region'].unique()[i], 'AveragePrice']
+    avgPofReg = torch.Tensor(avgPofReg)
+
+    for j in range(count_train - slide_win):
+        features = avgPofReg[j:j+slide_win]
+        label = avgPofReg[i+slide_win:i+slide_win+1]
+        train_seq.append((features, label))
+
+print(type(features))
+print(len(features))
+print(len(label))
+
+#train(train_seq, n_epochs, optimizer, lstm_model, loss_function)
 
 
 # 1. slidig window just for totalUS
