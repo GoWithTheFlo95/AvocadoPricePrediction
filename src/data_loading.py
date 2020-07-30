96e6b6a839341368d6c820e639285d237b1ffa50
Narges <narges.ghaedibardeh@Narges.GhaediBardeh@student.hpi.uni-potsdam.de>
2020-07-30 22:36:37 +0430

    change data loading

--- src/data_loading.py
+++ src/data_loading.py
@@ -23,6 +23,73 @@ sys.path.insert(0, home+'/models')
 from sklearn.model_selection import train_test_split
 
 
+
+
+## get processed data
+def getDataForRegression():
+	avocadoDat_unproc = pd.read_csv(home + '/../dataset/avocado.csv')
+
+	## Data preprocessing
+	# Removing unnecessary features
+	avocadoDat_proc = avocadoDat_unproc.iloc[:,[1,2]]
+	#print(avocadoDat_proc.head())
+
+	# Date transformation
+	avocadoDat_proc['Date'] = pd.to_datetime(avocadoDat_proc['Date'])
+
+	# Set Date as an index
+	#avocadoDat_proc = avocadoDat_proc.set_index('Date')
+	#print(avocadoDat_proc.head())
+
+	# Final, pre-processed dataset
+	avocadoDat = avocadoDat_proc
+	avocadoDat.sort_values(['Date'], inplace=True)
+	# Ser 
+
+	return avocadoDat
+
+
+## get all features for regression
+def getDataAllFeatures():
+	avocadoDat_unproc = pd.read_csv(home + '/../dataset/avocado.csv')
+	avocadoDat_proc=avocadoDat_unproc
+
+	list = []
+	for date in avocadoDat_proc.Date:
+		list.append(date.split("-"))
+		
+	# month and day adding to lists
+	month = []
+	day = []
+	for i in range(len(list)):
+		month.append(list[i][1])
+		day.append(list[i][2])
+		
+	# adding to dataset
+	avocadoDat_proc["month"] = month
+	avocadoDat_proc["day"] = day
+
+	#convert objects to int
+	avocadoDat_proc.month = avocadoDat_proc.month.values.astype(int)
+	avocadoDat_proc.day = avocadoDat_proc.day.values.astype(int)
+
+	# Renam Avocado Size Columsn
+	avocadoDat_proc = avocadoDat_proc.rename(index=str, columns={"Total Volume" : "TotalVol", "4046" : "SmallHass", "4225" : "LargeHass", "4770" : "XLargeHass", "Total Bags" : "TotalBags", "Small Bags" : "SmallBags", "Large Bags" : "LargeBags", "XLarge Bags" : "XLargeBags"})
+	
+	# drop unnecessary features
+	avocadoDat_proc.drop(["Unnamed: 0"],axis=1,inplace=True)	
+	avocadoDat_proc.drop(["Date"],axis=1,inplace=True)
+
+	avocadoDat_proc = pd.get_dummies(avocadoDat_proc,drop_first=True)
+	#pd.get_dummies(avocadoDat_proc,drop_first=False)
+
+	# Final, pre-processed dataset
+	avocadoDat = avocadoDat_proc
+
+	return avocadoDat
+
+
+
 ## get processed data
 def getData():
 	avocadoDat_unproc = pd.read_csv(home + '/../dataset/avocado.csv')
