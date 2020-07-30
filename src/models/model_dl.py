745fa7cf2925f097c339902e12e6d24cd54a3629
Florian Hermes <florian.hermes@student.hpi.uni-potsdam.de>
2020-07-30 17:19:32 +0200

    Finalize model_dl.py

--- src/models/model_dl.py
+++ src/models/model_dl.py
@@ -6,7 +6,8 @@ Author : Florian Hermes, HPI
 E-mail : florian.hermes@student.hpi.de
 Date   : 2020-07-13
 	
-	Define BaseModel, RNN and LSTM model.
+	Define BaseModel, LSTM model and RNN model.
+    For this project we focused on LSTM model, therefore the code for RNN is just kept here for future work.
 	
 """
 
@@ -24,12 +25,13 @@ from torch.autograd import Variable
 
 
 
-## BaseModel
+## Base Model
 class BaseModel(nn.Module):
     def __init__(self, input_size, hidden_size, output_size, sequence_len, n_layer, cell):
 
         super(BaseModel, self).__init__()
         
+        # initialize NN variables
         self.hidden_size = hidden_size          # number of neurons that store information of previous inputs
         self.input_size = input_size            # size of a input element of a sequence
         self.output_size = output_size          # size of a output element
@@ -39,12 +41,14 @@ class BaseModel(nn.Module):
 
         batch_first = True
 
+        # RNN layer
         if cell == "RNN":
             self.model = nn.RNN(input_size = self.input_size, hidden_size = self.hidden_size, num_layers = self.n_layer, batch_first=batch_first) #atch_first = True)
         
         if cell == "LSTM":
             self.model = nn.LSTM(input_size = self.input_size, hidden_size = self.hidden_size, num_layers = self.n_layer, batch_first=batch_first) #batch_first = True)
         
+        # linear layer
         self.linear = nn.Linear(self.hidden_size, self.output_size)
 
     def init_hidden(self, batch_size):
@@ -52,24 +56,8 @@ class BaseModel(nn.Module):
         hidden=hidden.to(self.device)
         return hidden
 
-# RNNModel
-class RNNModel(BaseModel):
-    def __init__(self, input_size, hidden_size, output_size, sequence_len, n_layer, cell):
 
-        super(RNNModel, self).__init__(input_size, hidden_size, output_size, sequence_len, n_layer, cell)
-
-    def forward(self, input):
-        batch_size = input.shape[0]
-        
-        h0 = self.init_hidden(batch_size)
-        
-        rnnOut, _hn = self.model(input, h0)
-
-        rnnOut = self.linear(rnnOut[:, -1, :])
-
-        return rnnOut
-
-# LSTMModel
+# LSTM Model
 class LSTMModel(BaseModel):
     def __init__(self, input_size, hidden_size, output_size, sequence_len, n_layer, cell):
 
@@ -87,3 +75,20 @@ class LSTMModel(BaseModel):
 
         return lstmOut
 
+
+# RNN Model
+class RNNModel(BaseModel):
+    def __init__(self, input_size, hidden_size, output_size, sequence_len, n_layer, cell):
+
+        super(RNNModel, self).__init__(input_size, hidden_size, output_size, sequence_len, n_layer, cell)
+
+    def forward(self, input):
+        batch_size = input.shape[0]
+        
+        h0 = self.init_hidden(batch_size)
+        
+        rnnOut, _hn = self.model(input, h0)
+
+        rnnOut = self.linear(rnnOut[:, -1, :])
+
+        return rnnOut
